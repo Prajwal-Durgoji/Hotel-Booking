aedb2f3ae7aeb818043c527dd8ac147b0a6d70fa
Prajwal <prajwaldurgoji16@gmail.com>
2024-09-02 00:09:24 +0530

    functions

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,24 +1,18 @@
-import React, { PureComponent } from 'react'
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './SearchBox.css';
 
-class SearchBoxClass extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            location: '',
-            checkInDate: '',
-            checkOutDate: '',
-            cities: [], 
-            searchQuery: '' 
-        };
-    }
+const SearchBox = () => {
+    const navigate = useNavigate();
+    const [location, setLocation] = useState('');
+    const [checkInDate, setCheckInDate] = useState('');
+    const [checkOutDate, setCheckOutDate] = useState('');
+    const [cities, setCities] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [loading, setLoading] = useState(false); 
+    const searchInputRef = useRef(null);
 
-    componentDidMount() {
-        this.fetchCities();
-    }
-
-    fetchCities = async () => {
+    const fetchCities = useCallback(async () => {
         try {
             const response = await fetch('https://api.countrystatecity.in/v1/countries/IN/cities', {
                 headers: {
@@ -29,29 +23,38 @@ class SearchBoxClass extends PureComponent {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
             const data = await response.json();
-            this.setState({ cities: data });
+            setCities(data);
         } catch (error) {
             console.error("Failed to fetch cities:", error);
         }
-    }
+    }, []);
 
-    handleChange = (event) => {
+    useEffect(() => {
+        fetchCities();
+    }, [fetchCities]);
+
+    const handleChange = useCallback((event) => {
         const { name, value } = event.target;
-        this.setState({ [name]: value });
-    }
+        if (name === 'checkInDate') setCheckInDate(value);
+        if (name === 'checkOutDate') setCheckOutDate(value);
+    }, []);
 
-    handleSearchChange = (event) => {
-        this.setState({ searchQuery: event.target.value });
-    }
+    const handleSearchChange = useCallback((event) => {
+        setSearchQuery(event.target.value);
+    }, []);
 
-    handleCitySelect = (city) => {
-        this.setState({ location: city, searchQuery: city });
-    }
+    const handleCitySelect = useCallback((city) => {
+        setLocation(city);
+        setSearchQuery(city);
+        if (searchInputRef.current) {
+            searchInputRef.current.blur();
+        }
+    }, []);
 
-    handleClick = async () => {
-        const { location, checkInDate, checkOutDate } = this.state;
-        const checkInDateFormatted = this.formatDate(checkInDate);
-        const checkOutDateFormatted = this.formatDate(checkOutDate);
+    const handleClick = useCallback(async () => {
+        setLoading(true); 
+        const checkInDateFormatted = formatDate(checkInDate);
+        const checkOutDateFormatted = formatDate(checkOutDate);
         console.log(`Formatted Dates: Check-In: ${checkInDateFormatted}, Check-Out: ${checkOutDateFormatted}, location: ${location}`);
 
         try {
@@ -69,110 +72,104 @@ class SearchBoxClass extends PureComponent {
             const weatherData = await weatherResponse.json();
             console.log(`Weather in ${location}: ${weatherData.weather[0].main}, ${weatherData.main.temp}°C`);
 
-            this.props.navigate("/home", { state: { hotels: data, weather: weatherData } });
+            navigate("/home", { state: { hotels: data, weather: weatherData } });
         } catch (error) {
             console.error("Failed to fetch hotels or weather:", error);
+        } finally {
+            setLoading(false); 
         }
-    }
+    }, [location, checkInDate, checkOutDate, navigate]);
 
-    formatDate = (dateString) => {
+    const formatDate = (dateString) => {
         const date = new Date(dateString);
         const day = String(date.getDate()).padStart(2, '0');
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const year = date.getFullYear();
         return `${day}/${month}/${year}`;
-    }
+    };
 
-    render() {
-        const { cities, searchQuery } = this.state;
-        const filteredCities = cities.filter(city => city.name.toLowerCase().includes(searchQuery.toLowerCase()));
+    const filteredCities = cities.filter(city => city.name.toLowerCase().includes(searchQuery.toLowerCase()));
 
-        return (
-            <>
-                <div className="search-box__container">
-                    <h1>LET'S GO!<br />THE <span>ADVENTURE</span> IS<br /> WAITING FOR YOU</h1>
-                    <p>
-                        Embark on Your Journey Today and Discover Uncharted Wonders Around the
-                        World - Your Adventure Awaits with Exciting Experiences, Unforgettable
-                        Memories, and Endless Opportunities
-                    </p>
-                    <div className="search-box__input">
+    return (
+        <>
+            <div className="search-box__container">
+                <h1>LET'S GO!<br />THE <span>ADVENTURE</span> IS<br /> WAITING FOR YOU</h1>
+                <p>
+                    Embark on Your Journey Today and Discover Uncharted Wonders Around the
+                    World - Your Adventure Awaits with Exciting Experiences, Unforgettable
+                    Memories, and Endless Opportunities
+                </p>
+                <div className="search-box__input">
+                    <div className="input-group">
+                        <label>Where</label>
+                        <input
+                            type="text"
+                            name="location"
+                            value={searchQuery}
+                            placeholder="Enter City"
+                            onChange={handleSearchChange}
+                            autoComplete="off"
+                            ref={searchInputRef}
+                        />
+                        {searchQuery && (
+                            <ul className="dropdown">
+                                <div className="dropdown-inner">
+                                    {filteredCities.map(city => {
+                                        let cityName = city.name;
+                                        if (cityName === "Bangalore Rural" || cityName === "Bangalore Urban") {
+                                            cityName = "Bangalore";
+                                        }
+                                        return (
+                                            <li key={city.id} onClick={() => handleCitySelect(`${cityName},IN`)}>
+                                                <i className="fas fa-map-marker-alt location-icon"></i> {cityName},IN
+                                            </li>
+                                        );
+                                    })}
+                                </div>
+                            </ul>
+                        )}
+                    </div>
+                    <div className="date-inputs">
                         <div className="input-group">
-                            <label>Where</label>
-                            <input
-                                type="text"
-                                name="location"
-                                value={searchQuery}
-                                placeholder="Enter City"
-                                onChange={this.handleSearchChange}
-                                autoComplete="off"
-                            />
-                            {searchQuery && (
-                                <ul className="dropdown">
-                                    <div className="dropdown-inner">
-                                        {filteredCities.map(city => {
-                                            let cityName = city.name;
-                                            if (cityName === "Bangalore Rural" || cityName === "Bangalore Urban") {
-                                                cityName = "Bangalore";
-                                            }
-                                            return (
-                                                <li key={city.id} onClick={() => this.handleCitySelect(`${cityName},IN`)}>
-                                                    <i className="fas fa-map-marker-alt location-icon"></i> {cityName},IN
-                                                </li>
-                                            );
-                                        })}
-                                    </div>
-                                </ul>
-                            )}
+                            <label>Check-in</label>
+                            <input type="date" name="checkInDate" placeholder="Check-in date" onChange={handleChange} />
                         </div>
-                        <div className="date-inputs">
-                            <div className="input-group">
-                                <label>Check-in</label>
-                                <input type="date" name="checkInDate" placeholder="Check-in date" onChange={this.handleChange} />
-                            </div>
-                            <div className="input-group">
-                                <label>Check-out</label>
-                                <input type="date" name="checkOutDate" placeholder="Check-out date" onChange={this.handleChange} />
-                            </div>
-                        </div>
-                        <button type="button" className='search-button' onClick={this.handleClick}>
-                            Search
-                        </button>
-                    </div>
-                </div>
-
-                <div className="travel-container">
-                    <img src="/images/header-bg.png" alt="Header-Bg" />
-                    <img className="outside-image" src="/images/header.png" alt="Another Header-Bg" />
-                    <div className="header__image">
-                        <div className="header__image__card header__image__card-1">
-                            <span><i className="ri-key-line"></i></span>
-                            Hotel Booking
-                        </div>
-                        <div className="header__image__card header__image__card-2">
-                            <span><i className="ri-passport-line"></i></span>
-                            Flight Tickets
-                        </div>
-                        <div className="header__image__card header__image__card-3">
-                            <span><i className="ri-map-2-line"></i></span>
-                            Local Events
-                        </div>
-                        <div className="header__image__card header__image__card-4">
-                            <span><i className="ri-guide-line"></i></span>
-                            Tour Guide
+                        <div className="input-group">
+                            <label>Check-out</label>
+                            <input type="date" name="checkOutDate" placeholder="Check-out date" onChange={handleChange} />
                         </div>
                     </div>
+                    <button type="button" className='search-button' onClick={handleClick} disabled={loading}>
+                        {loading ? 'Loading...' : 'Search'}
+                    </button>
                 </div>
-            </>
-        );
-    }
-}
+            </div>
 
-const SearchBox = () => {
-    const navigate = useNavigate();
-    return <SearchBoxClass navigate={navigate} />;
-}
-
+            <div className="travel-container">
+                <img src="/images/header-bg.png" alt="Header-Bg" />
+                <img className="outside-image" src="/images/header.png" alt="Another Header-Bg" />
+                <div className="header__image">
+                    <div className="header__image__card header__image__card-1">
+                        <span><i className="ri-key-line"></i></span>
+                        Hotel Booking
+                    </div>
+                    <div className="header__image__card header__image__card-2">
+                        <span><i className="ri-passport-line"></i></span>
+                        Flight Tickets
+                    </div>
+                    <div className="header__image__card header__image__card-3">
+                        <span><i className="ri-map-2-line"></i></span>
+                        Local Events
+                    </div>
+                    <div className="header__image__card header__image__card-4">
+                        <span><i className="ri-guide-line"></i></span>
+                        Tour Guide
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
 export default SearchBox;
 
 
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,101 +3,101 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import './HomePage.css';
 
 const HomePage = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const [hotels, setHotels] = useState([]);
-  const [weather, setWeather] = useState(null);
-  const [filters, setFilters] = useState({
-    priceRange: [0, 10000],
-    roomOption: ''
-  });
-  const [currentPriceRange, setCurrentPriceRange] = useState(10000);
-
-  useEffect(() => {
-    const locationState = location.state;
-    if (locationState) {
-      setHotels(locationState.hotels);
-      setWeather(locationState.weather);
-      console.log("HomePage state:", locationState);
-      console.log("Weather state:", locationState.weather);
-      console.log("Location state:", locationState.location);
-    }
-  }, [location.state]);
-
-  const handleHotelClick = useCallback((hotelId) => {
-    navigate("/hotel-details", { state: hotelId });
-    console.log("Navigating to hotel details for hotelId:", hotelId);
-  }, [navigate]);
-
-  const handleFilterChange = useCallback((filterName, value) => {
-    setFilters(prevFilters => ({
-      ...prevFilters,
-      [filterName]: value
-    }));
-    if (filterName === 'priceRange') {
-      setCurrentPriceRange(value[1]);
-    }
-    console.log("Filters updated:", filters);
-  }, [filters]);
-
-  const applyFilters = useCallback(() => {
-    return hotels.filter(hotel => {
-      const priceMatch = hotel.price >= filters.priceRange[0] && hotel.price <= filters.priceRange[1];
-      const roomMatch = filters.roomOption ? hotel.roomOption === filters.roomOption : true;
-      console.log("Filtering hotel:", hotel.name, { priceMatch, roomMatch });
-      return priceMatch && roomMatch;
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [hotels, setHotels] = useState([]);
+    const [weather, setWeather] = useState(null);
+    const [filters, setFilters] = useState({
+        priceRange: [0, 10000],
+        roomOption: ''
     });
-  }, [hotels, filters]);
+    const [currentPriceRange, setCurrentPriceRange] = useState(10000);
 
-  const filteredHotels = applyFilters();
-  console.log("Filtered hotels:", filteredHotels);
+    useEffect(() => {
+        const locationState = location.state;
+        if (locationState) {
+            setHotels(locationState.hotels);
+            setWeather(locationState.weather);
+            console.log("HomePage state:", locationState);
+            console.log("Weather state:", locationState.weather);
+        }
+    }, [location]);
 
-  return (
-    <>
-      <div className="filters">
-        <label>Price Range:
-          <input type="range" min="0" max="10000" value={filters.priceRange[1]} onChange={(e) => handleFilterChange('priceRange', [filters.priceRange[0], e.target.value])} />
-          <span className="price-range-value">₹{currentPriceRange}</span> {/* Display the current price range */}
-        </label>
-        <label>Room Option:
-          <select value={filters.roomOption} onChange={(e) => handleFilterChange('roomOption', e.target.value)}>
-            <option value="">All</option>
-            <option value="1BHK">1BHK</option>
-            <option value="2BHK">2BHK</option>
-            <option value="3BHK">3BHK</option>
-            <option value="4BHK">4BHK</option>
-          </select>
-        </label>
-        {weather && (
-          <div className="weather-info">
-            <h2>Weather in {weather.name}: {weather.weather[0].main}</h2>
-            <p>Temperature: {weather.main.temp}°C</p>
-            <p>Humidity: {weather.main.humidity}%</p>
-          </div>
-        )}
-      </div>
-      <div className="container-hotel">
-        {Array.isArray(filteredHotels) && filteredHotels.map((hotel, index) => (
-          <div key={index} className="hotel-card" onClick={() => handleHotelClick(hotel.id)}>
-            <img src={`http://localhost:8080/${hotel.imageUrl}`} alt={hotel.hotelName} />
-            <div className="hotel-info">
-              <h2>{hotel.hotelName}</h2>
-              <p>Price: ₹{hotel.price}</p>
-              <p>Rooms: {hotel.roomOption}</p>
-              <div className="nearby-container">
-                {Array.from(hotel.nearby).map((place, index) => (
-                  <div key={index} className="nearby-place">
-                    {place.place}
-                  </div>
-                ))}
-              </div>
-              <p>Area: {hotel.area}</p>
+    const handleHotelClick = useCallback((hotelId) => {
+        navigate("/hotel-details", { state: hotelId });
+        console.log("Navigating to hotel details for hotelId:", hotelId);
+    }, [navigate]);
+
+    const handleFilterChange = useCallback((filterName, value) => {
+        setFilters(prevFilters => ({
+            ...prevFilters,
+            [filterName]: value
+        }));
+        if (filterName === 'priceRange') {
+            setCurrentPriceRange(value[1]);
+        }
+        console.log("Filters updated:", filters);
+
+    }, [filters]);
+
+    const applyFilters = useCallback(() => {
+        return hotels.filter(hotel => {
+            const priceMatch = hotel.price >= filters.priceRange[0] && hotel.price <= filters.priceRange[1];
+            const roomMatch = filters.roomOption ? hotel.roomOption === filters.roomOption : true;
+            console.log("Filtering hotel:", hotel.name, { priceMatch, roomMatch });
+            return priceMatch && roomMatch;
+        });
+    }, [hotels, filters]);
+
+    const filteredHotels = applyFilters();
+    console.log("Filtered hotels:", filteredHotels);
+
+    return (
+        <>
+            <div className="filters">
+                <label>Price Range:
+                    <input type="range" min="0" max="10000" value={filters.priceRange[1]} onChange={(e) => handleFilterChange('priceRange', [filters.priceRange[0], e.target.value])} />
+                    <span className="price-range-value">₹{currentPriceRange}</span> {/* Display the current price range */}
+                </label>
+                <label>Room Option:
+                    <select value={filters.roomOption} onChange={(e) => handleFilterChange('roomOption', e.target.value)}>
+                        <option value="">All</option>
+                        <option value="1BHK">1BHK</option>
+                        <option value="2BHK">2BHK</option>
+                        <option value="3BHK">3BHK</option>
+                        <option value="4BHK">4BHK</option>
+                    </select>
+                </label>
+                {weather && (
+                    <div className="weather-info">
+                        <h2>Weather in {weather.name}: {weather.weather[0].main}</h2>
+                        <p>Temperature: {weather.main.temp}°C</p>
+                        <p>Humidity: {weather.main.humidity}%</p>
+                    </div>
+                )}
             </div>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+            <div className="container-hotel">
+                {Array.isArray(filteredHotels) && filteredHotels.map((hotel, index) => (
+                    <div key={index} className="hotel-card" onClick={() => handleHotelClick(hotel.id)}>
+                        <img src={`http://localhost:8080/${hotel.imageUrl}`} alt={hotel.hotelName} />
+                        <div className="hotel-info">
+                            <h2>{hotel.hotelName}</h2>
+                            <p>Price: ₹{hotel.price}</p>
+                            <p>Rooms: {hotel.roomOption}</p>
+                            <div className="nearby-container">
+                                {Array.from(hotel.nearby).map((place, index) => (
+                                    <div key={index} className="nearby-place">
+                                        {place.place}
+                                    </div>
+                                ))}
+                            </div>
+                            <p>Area: {hotel.area}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default HomePage;
